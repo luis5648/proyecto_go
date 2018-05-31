1dbfb024ef621d09a5784ffde971bd597a5ed1dd
Luis Román <porta5648@gmail.com>
2018-05-31 06:57:28 -0500

    Variable para hacer Scan

--- files/Main.go
+++ files/Main.go
@@ -3,7 +3,18 @@ package main
 import "fmt"
 
 func main(){
+
+	var a int
+	fmt.Println("Introduzca el valor de a: ")
+	fmt.Scanln(&a)
+
+
 	for i:= 0; i < 10; i++{
+		fmt.Println(a,", ")
+		
 		fmt.Println("Hello world")
+
+		a++
+
 	}
 }
